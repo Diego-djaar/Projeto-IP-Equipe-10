a5fd5b9a62082adb8e3176edf1293df896613656
Diego <djaar@cin.ufpe.br>
2022-10-16 22:31:25 -0300

    Carregar imagens de arquivos apenas uma vez para Planet

--- src/planet.py
+++ src/planet.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 import math
 from random import randint, choice
 from . import display
@@ -7,17 +8,26 @@ from . import display
 PLANET_GROUP: pygame.sprite.Group
 PLANET_TIMER: int
 PLANET_RECT_LIST = []
+PLANET_0_IMAGE: pygame.Surface = None
+PLANET_1_IMAGE: pygame.Surface = None
 
 
 class Planet(pygame.sprite.Sprite):
     def __init__(self, typo):
         super().__init__()
+        current_module = sys.modules[__name__]
+
         if typo == 'small':
-            self.image = pygame.image.load('graphics/planet/planet_0.png').convert_alpha()
+            if current_module.PLANET_0_IMAGE is None:
+                current_module.PLANET_0_IMAGE = self.image = pygame.image.load('graphics/planet/planet_0.png').convert_alpha()
+            self.image = current_module.PLANET_0_IMAGE
             self.image = pygame.transform.rotozoom(self.image, 0, 0.4)
             self.speed = 6
+
         elif typo == 'medium':
-            self.image = pygame.image.load('graphics/planet/planet_1.png').convert_alpha()
+            if current_module.PLANET_1_IMAGE is None:
+                current_module.PLANET_1_IMAGE = self.image = pygame.image.load('graphics/planet/planet_1.png').convert_alpha()
+            self.image = current_module.PLANET_1_IMAGE
             self.image = pygame.transform.rotozoom(self.image, 0, 0.6)
             self.speed = 5
         # elif type == 'large':
